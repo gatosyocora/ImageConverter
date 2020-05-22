5931fa0e0cc7484f3995e1ffdeccb059ddf9b477
gatosyocora <gatosyocora36@gmail.com>
2020-05-22 19:03:48 +0900

    ロジック部分を分離

---
+++ ImageConverter/ImageConverterLogic.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Text;
+
+namespace Gatosyocora.ImageConverter.Logic
+{
+    static class ImageConverterLogic
+    {
+        /// <summary>
+        /// ファイルパスから画像を読み込む
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <returns></returns>
+        private static Image LoadImage(string filePath)
+        {
+            if (string.IsNullOrEmpty(filePath))
+            {
+                throw new Exception("ファイルパスが設定されていません");
+            }
+
+            if (!File.Exists(filePath))
+            {
+                throw new FileNotFoundException("ファイルが見つかりません");
+            }
+
+            Image srcImage;
+
+            using (var stream = new FileStream(filePath, FileMode.Open, FileAccess.ReadWrite))
+            {
+                srcImage = Image.FromStream(stream);
+            }
+            return srcImage;
+        }
+
+        /// <summary>
+        /// 画像を保存する
+        /// </summary>
+        /// <param name="image"></param>
+        /// <param name="filePath"></param>
+        private static void SaveImage(Image image, string filePath)
+        {
+            if (image != null)
+            {
+                throw new NullReferenceException();
+            }
+
+            if (string.IsNullOrEmpty(filePath))
+            {
+                throw new Exception("ファイルパスが設定されていません");
+            }
+
+            image.Save(filePath, ImageFormat.Png);
+            image.Dispose();
+        }
+
+        /// <summary>
+        /// 画像を水平方向に反転させる
+        /// </summary>
+        /// <param name="filePath">画像のフルパス</param>
+        public static void FlipHorizontal(string filePath)
+        {
+            var image = LoadImage(filePath);
+            FlipHorizontal(image);
+            SaveImage(image, filePath);
+        }
+
+        /// <summary>
+        /// 画像を水平方向に反転させる
+        /// </summary>
+        /// <param name="filePath"></param>
+        private static void FlipHorizontal(Image image)
+        {
+            if (image != null)
+            {
+                throw new NullReferenceException();
+            }
+
+            image.RotateFlip(RotateFlipType.Rotate180FlipY);
+        }
+    }
+}
--- ImageConverter/Program.cs
+++ ImageConverter/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System.IO;
 using System.Drawing;
 using System.Drawing.Imaging;
+using Gatosyocora.ImageConverter.Logic;
 
 namespace Gatosyocora.ImageConverter
 {
@@ -10,21 +11,7 @@ namespace Gatosyocora.ImageConverter
         {
             if (args?.Length > 0 && string.IsNullOrEmpty(args[0])) return;
 
-            FlipY(args[0]);
-        }
-
-        private static void FlipY(string filePath)
-        {
-            if (string.IsNullOrEmpty(filePath)) return;
-
-            Image srcImage;
-            using (var stream = new FileStream(filePath, FileMode.Open, FileAccess.ReadWrite))
-            {
-                srcImage = Image.FromStream(stream);
-            }
-            srcImage.RotateFlip(RotateFlipType.Rotate180FlipY);
-            srcImage.Save(filePath, ImageFormat.Png);
-            srcImage.Dispose();
+            ImageConverterLogic.FlipHorizontal(args[0]);
         }
     }
 }
